f0607cce50415ae03d61aeb5b59c92a81cb780f6
jpfeijo <j.feijo@edu.pucrs.br>
2022-12-19 15:32:53 -0300

    fashion mnist

--- fashion_mnist.py
+++ fashion_mnist.py
@@ -1,54 +1,42 @@
 import tensorflow as tf
-import tensorflow_datasets as tfds
+from tensorflow import keras
+import matplotlib.pyplot as plt
 
-(ds_train, ds_test), ds_info = tfds.load(
-    'fashion_mnist',
-    split=['train', 'test'],
-    shuffle_files=True,
-    as_supervised=True,
-    with_info=True
-)
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
+test_images = test_images / 255.0
+train_images = train_images / 255.0
 
-# Normalizes the images 'uint8' -> 'float32'
-def normalize_img(image, label):
-    return tf.cast(image, tf.float32) / 255., label
-
-ds_train = ds_train.map(
-    normalize_img, num_parallel_calls=tf.data.AUTOTUNE # Fornece imagens do tipo 'uint8' que vão ser normalizadas
-)
-ds_train = ds_train.cache() # Conforme ajusta os dados na memória, vai armazenando em cache antes de embaralhar para melhor desempenho
-ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples) # Embaralha os dados
-ds_train = ds_train.batch(128) # Divide os dados em lotes de 128 imagens
-ds_train = ds_train.prefetch(tf.data.AUTOTUNE) # Boa prática encerrar o modelo por pre busca para desempenho
-
-# Criar um pipeline de avaliação
-ds_test = ds_test.map(
-    normalize_img, num_parallel_calls=tf.data.AUTOTUNE
-)
-ds_test = ds_test.batch(128)
-ds_test = ds_test.cache()
-ds_test = ds_test.prefetch(tf.data.AUTOTUNE)
-
-# Criar o modelo
-model = tf.keras.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28,28)),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dense(10)
+model = keras.Sequential([
+    # Todas as imagens devem ser inseridas no mesmo tamanho e tipo
+    keras.layers.Flatten(input_shape=(28,28)), # Sem neuronios, apenas transforma a imagem que foi inputada em um array
+    keras.layers.Dense(128, activation='relu'),
+    #keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10, activation='softmax') # 10 neuronios, uma para cada tipo de classe -> Softmax ajdua a encontrar a classe que está mais próxima
 ])
 
-# Compilando o modelo
 model.compile(
-    optimizer=tf.keras.optimizers.Adam(0.001),
-    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
     metrics=['accuracy']
-    #tf.keras.metrics.SparseCategoricalAccuracy()
 )
 
-# Treinando o modelo
-model.fit(
-    ds_train,
+model_fit = model.fit(
+    train_images,
+    train_labels,
     epochs=10,
-    validation_data=ds_test
+    batch_size=100,
+    verbose = 1 # 0 = silent, 1 = progress bar, 2 = one line per epoch
 )
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+plt.plot(model_fit.history['accuracy'])
+plt.plot(model_fit.history['loss'])
+plt.title('Model Accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.show()
+
+#predictions = model.predict(test_images)
